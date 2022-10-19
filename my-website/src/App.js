9ffc2821ec11a782972193db43f3dd4304621bda
behmnoah <57685424+behmnoah@users.noreply.github.com>
2022-10-19 02:46:46 -0400

    adjusted css, fixed navbar, added footer

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -7,39 +7,42 @@ import Navbar from "./Navigation/Navbar.js";
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar className = 'nav-bar'/>
       <header className="App-header">
 
-
-        <div className="container-column">
-
-          <div>
-            <div className="container-row">
+          <div className="container-row">
+            <div className="container-column">
               <div className="p-2">        
-                <p className="Name-Text">
-                  Hello I'm Noah Behm    
+                  <p className="Name-Text">
+                    Hello I'm Noah Behm    
+                  </p>
+              </div>
+              <div className="p-2">
+                <p className='small-text'>
+                  I'm a computer science student at Michigan State University, graduating in 2022. 
+                  Look around my page to learn more about me and see what I'm up to!
                 </p>
               </div>
-              <div className="p-2">   
-                  <ReactRoundedImage image={logo} roundedSize="1" imageWidth="230" imageHeight="230" />
-              </div>
+            </div>
+
+
+
+
+            <div className="p-2">   
+                <ReactRoundedImage image={logo} roundedSize="0" imageWidth="350" imageHeight="350" roundedColor="#dddcf194"/>
             </div>
           </div>
 
-          <div>
-              <div className="p-2">        
-                <p className='small-text'>
-                  I'm a computer science student at Michigan State University, graduating in 2022. This is a placeholder text which will be filled in later with something better. 
-                </p>
-              </div>
-          </div>
-
-
-
-
-        </div>
       </header>
-
+      <hr className='head-foot-line'></hr>
+      <footer className='contact-info'>
+        <p className='footer-text'>
+          Contact me!
+        </p>
+        <p className='footer-text'>
+          behmnoah@gmail.com/behmnoah@msu.edu 
+        </p>
+      </footer>
 
 
     </div>
--- my-website/src/Navigation/Navbar.js
+++ my-website/src/Navigation/Navbar.js
@@ -1,32 +1,26 @@
 import React from "react";
-
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
+    <div>
+        <nav className="nav-bar">
 
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Projects
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          <a className="navbar-brand" href="/Home">
+            Home
+          </a>
+
+          <a className="navbar-brand" href="/About">
+            About
+          </a>
+
+
+          <a className="navbar-brand" href="/Projects">
+            Projects
+          </a>
+
+      </nav>
+    </div>
+    
   );
 };
 
